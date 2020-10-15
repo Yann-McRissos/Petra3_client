1c98585bb2f90437928fde7239e79e7ad6d4388a
Yann-McRissos <citrouillon@skynet.be>
2020-10-15 10:19:39 +0200

    :tada: Initial commit. Almost done just gotta create a thread to receive data

---
+++ src/Petra2_client/Petra2_client_main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Petra2_client;
+
+/**
+ *
+ * @author jerem
+ */
+public class Petra2_client_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Petra_MainWindow mw = new Petra_MainWindow();
+        mw.setVisible(true);
+    }
+    
+}
